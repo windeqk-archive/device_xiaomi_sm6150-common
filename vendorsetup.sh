a9f57e11208b8f518c15887f2a02c55ab9f67c6b
windeqk <trojan.windows.11@gmail.com>
2021-07-09 12:16:47 +0200

    sm6150: Repar XiaomiDoze
    Error:
    [  0% 228/99050] //device/xiaomi/sm6150-common/doze:XiaomiDoze dexpreopt [common]
    FAILED: /home/windeqk/p404/out/soong/.intermediates/device/xiaomi/sm6150-common/doze/XiaomiDoze/android_common/dexpreopt/oat/arm64/package.invocation /home/windeqk/p404/out/soong/.intermediates/device/xiaomi/sm6150-common/doze/XiaomiDoze/android_common/dexpreopt/oat/arm64/package.odex /home/windeqk/p404/out/soong/.intermediates/device/xiaomi/sm6150-common/doze/XiaomiDoze/android_common/dexpreopt/oat/arm64/package.vdex
    echo "module XiaomiDoze missing dependencies: org.lineageos.settings.resources" && false
    module XiaomiDoze missing dependencies: org.lineageos.settings.resources
    [  0% 229/99050] //device/xiaomi/sm6150-common/doze:XiaomiDoze signapk [common]
    FAILED: /home/windeqk/p404/out/soong/.intermediates/device/xiaomi/sm6150-common/doze/XiaomiDoze/android_common/XiaomiDoze.apk
    echo "module XiaomiDoze missing dependencies: org.lineageos.settings.resources" && false
    module XiaomiDoze missing dependencies: org.lineageos.settings.resources
    
    Signed-off-by: windeqk <71609034+windeqk@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,6 @@
 # proton
 #rm -rf prebuilts/clang/host/linux-x86/clang-proton
 git clone https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/clang-proton --depth=1
+
+# Clone devicesettings
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-18.1 packages/resources/devicesettings --depth=1
