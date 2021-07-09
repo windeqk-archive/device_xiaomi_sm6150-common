b2cbd07d34008cc80139e6fbb8bd9fc4edff82d4
windeqk <trojan.windows.11@gmail.com>
2021-07-09 12:23:39 +0200

    sm6150: Add some to manifest.xml and change android.hardware.graphics.composer
    Error:
    checkvintf E 07-07 18:11:08 134136 134136 check_vintf.cpp:554] files are incompatible: Device manifest and
     framework compatibility matrix are incompatible: HALs incompatible. Matrix level = 4. Manifest level = 4.
     The following requirements are not met:
    checkvintf E 07-07 18:11:08 134136 134136 check_vintf.cpp:554] android.hardware.graphics.allocator:
    checkvintf E 07-07 18:11:08 134136 134136 check_vintf.cpp:554]     required:
    checkvintf E 07-07 18:11:08 134136 134136 check_vintf.cpp:554]         @2.0::IAllocator/default OR
    checkvintf E 07-07 18:11:08 134136 134136 check_vintf.cpp:554]         @3.0::IAllocator/default OR
    checkvintf E 07-07 18:11:08 134136 134136 check_vintf.cpp:554]         @4.0::IAllocator/default
    checkvintf E 07-07 18:11:08 134136 134136 check_vintf.cpp:554]     provided:
    checkvintf E 07-07 18:11:08 134136 134136 check_vintf.cpp:554] android.hardware.graphics.composer:
    checkvintf E 07-07 18:11:08 134136 134136 check_vintf.cpp:554]     required: @2.1-4::IComposer/default
    checkvintf E 07-07 18:11:08 134136 134136 check_vintf.cpp:554]     provided:
    checkvintf E 07-07 18:11:08 134136 134136 check_vintf.cpp:554] android.hardware.graphics.mapper:
    checkvintf E 07-07 18:11:08 134136 134136 check_vintf.cpp:554]     required:
    checkvintf E 07-07 18:11:08 134136 134136 check_vintf.cpp:554]         @2.1::IMapper/default OR
    checkvintf E 07-07 18:11:08 134136 134136 check_vintf.cpp:554]         @3.0::IMapper/default OR
    checkvintf E 07-07 18:11:08 134136 134136 check_vintf.cpp:554]         @4.0::IMapper/default
    checkvintf E 07-07 18:11:08 134136 134136 check_vintf.cpp:554]     provided:
    checkvintf E 07-07 18:11:08 134136 134136 check_vintf.cpp:554] : Success
    INCOMPATIBLE
    
    Signed-off-by: windeqk <71609034+windeqk@users.noreply.github.com>

--- sm6150.mk
+++ sm6150.mk
@@ -107,7 +107,7 @@ PRODUCT_COPY_FILES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.4-service \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
